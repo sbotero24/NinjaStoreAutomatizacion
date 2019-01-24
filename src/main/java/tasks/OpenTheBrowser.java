41474aaf02297b4d7448498a734d67f210c2d694
Felipe Perez <fperezupegui@gmail.com>
2019-01-24 10:35:17 -0500

    actualizacion

---
+++ src/main/java/tasks/OpenTheBrowser.java
@@ -0,0 +1,28 @@
+package tasks;
+
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Open;
+
+public class OpenTheBrowser implements Task{
+	
+	private PageObject page;
+	
+	public OpenTheBrowser(PageObject page) {
+		this.page = page;
+	}
+
+	@Override
+	public <T extends Actor> void performAs(T actor) {
+		
+		actor.attemptsTo(Open.browserOn(page));
+	}
+
+	
+	public static OpenTheBrowser on(PageObject page) {
+		return instrumented(OpenTheBrowser.class, page);
+	}
+}
