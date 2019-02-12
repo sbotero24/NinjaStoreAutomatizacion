1b5cc903fccdc25cd9333cf464314f0ed0ff4c46
devco <sbotero24@hotmail.com>
2019-02-12 14:22:32 -0500

    actualizacion de repo ninjaStore

--- src/main/java/tasks/OpenTheBrowser.java
+++ src/main/java/tasks/OpenTheBrowser.java
@@ -6,23 +6,24 @@ import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Open;
+import net.thucydides.core.annotations.Step;
 
-public class OpenTheBrowser implements Task{
+public class OpenTheBrowser implements Task {
 	
 	private PageObject page;
 	
+	
 	public OpenTheBrowser(PageObject page) {
-		this.page = page;
+		this.page= page;
+		
 	}
-
 	@Override
 	public <T extends Actor> void performAs(T actor) {
-		
 		actor.attemptsTo(Open.browserOn(page));
+		
 	}
-
-	
 	public static OpenTheBrowser on(PageObject page) {
 		return instrumented(OpenTheBrowser.class, page);
 	}
+
 }
